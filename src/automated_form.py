72bd5b5e0a0cef1b974ef28fac853c45d0b1801f
Taiwo Omoya <taiwoomoya272@gmail.com>
2024-11-09 18:37:03 -0500

    This is the node.js file for the automated_form

--- src/automated_form.py
+++ src/automated_form.py
@@ -4,9 +4,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-#defines the web browsers 
+#defines the web browsers and wait
 driver = webdriver.Firefox()
 wait = WebDriverWait(driver, 10)
+
+first = "John"
+last_name = "Doe"
+phone_number = "1234567890"
+comments = "There is a pothole on the main road that needs urgent attention"
+
+
 
 try:
     driver.get("https://www.troyny.gov/FormCenter/Public-Works-Streets-9/Report-a-Pothole-55")
@@ -55,7 +62,24 @@ try:
     try:
         text_area = wait.until(EC.element_to_be_clickable((By.ID, 'e_7')))
     except Exception as e:
-        print(f"Could not finf text are e_7")
+        print(f"Could not find text are e_7: {e}")
+
+    try:
+        want_copy = wait.until(EC.element_to_be_clickable((By.ID, 'wantCopy')))
+    except Exception as e:
+        print(f"Could not find the wantCopy checkbox: {e}")
+        
+    try:
+        submit_button = wait.until(EC.element_to_be_clickable((By.ID, 'btnFormSubmit')))
+        submit_button.click()
+    except Exception as e:
+        print("Could not find the submit button:", e)
+
+    
+
+
+
+
 
 finally:
     driver.quit()
