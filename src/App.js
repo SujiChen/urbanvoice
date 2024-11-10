64257593a4c11243c7472209ce3b18774277a286
Taiwo Omoya <taiwoomoya272@gmail.com>
2024-11-09 23:25:23 -0500

    The form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import CoverPage from './CoverPage/CoverPage';
-import AuthProvider from './login/authprovider';
+import react from 'react'
+import "./App.css";
+import CoverPage from "./CoverPage/CoverPage";
+import AuthProvider from "./login/authprovider";
 import Profile from "./Profile/Profile";
-import { BrowserRouter as Router, Routes, Route }from 'react-router-dom';
-
-
-
-
+import Form from "./form/report-form";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <AuthProvider>
-    <div className="App">
-      {/* <header className="App-header">
+        <div className="App">
+          {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -28,23 +25,22 @@ function App() {
           Learn React
         </a>
       </header> */}
-      {/* Other components can access the user state via useAuth */}
-      {/* <CoverPage/> */}
+          {/* Other components can access the user state via useAuth */}
+          {/* <CoverPage/> */}
 
-      <Routes>
-         {/* Route for the root path */}
-          <Route path="/" element={<CoverPage />} />
-          
-          {/* Route for the profile page */}
-          <Route path="/profile" element={<Profile />}  />
-          
-          {/* Optional: Handle 404/Not Found */}
-          <Route path="*" element={<div>404 Not Found</div>} />
-      </Routes>
-    </div>
-    </AuthProvider>
+          <Routes>
+            {/* Route for the root path */}
+            <Route path="/" element={<CoverPage />} />
+
+            {/* Route for the profile page */}
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/report-form" element={<Form />} />
+            {/* Optional: Handle 404/Not Found */}
+            <Route path="*" element={<div>404 Not Found</div>} />
+          </Routes>
+        </div>
+      </AuthProvider>
     </Router>
-
   );
 }
 
--- src/CoverPage/CoverPage.jsx
+++ src/CoverPage/CoverPage.jsx
@@ -1,98 +1,91 @@
 import React, { useState, useEffect, useContext } from "react";
-import './CoverPage.css';
-import '../App.css';
-import logo from '../img/logo.svg';
-import LoginButtonComponent from '../login/LoginButtonComponent.jsx';
-import { auth, provider } from '../login/firebase.js';
-import { onAuthStateChanged } from 'firebase/auth';
-import { AuthContext } from '../login/authprovider'; 
-import { MegaphoneIcon, ClipboardListIcon, UsersIcon } from 'lucide-react';
+import "./CoverPage.css";
+import "../App.css";
+import logo from "../img/logo.svg";
+import LoginButtonComponent from "../login/LoginButtonComponent.jsx";
+import { auth, provider } from "../login/firebase.js";
+import { onAuthStateChanged } from "firebase/auth";
+import { AuthContext } from "../login/authprovider";
+import { Link } from "react-router-dom";
+import { MegaphoneIcon, ClipboardListIcon, UsersIcon } from "lucide-react";
 
-
-
-function CoverPage(){
-    const { user } = useContext(AuthContext);
-    return(
-   <div className="coverpage">
-        <div className="header">
-
-            <div className="left">
-                <img src={logo} alt="" />
-                <h1>  UrbanVoice </h1>
-            </div>
-          
-            <div className="right">
-                <h3>about</h3>
-                <h3>contact</h3>
-                <LoginButtonComponent />
-            </div>
+function CoverPage() {
+  const { user } = useContext(AuthContext);
+  return (
+    <div className="coverpage">
+      <div className="header">
+        <div className="left">
+          <img src={logo} alt="" />
+          <h1> UrbanVoice </h1>
         </div>
 
-        <div className="heroSection">
-            <h2>Make Your City Better </h2>
-            <h3>Voice your opinion and concerns to make your city a better place for everyone</h3>
+        <div className="right">
+          <h3>about</h3>
+          <h3>contact</h3>
+          <LoginButtonComponent />
+        </div>
+      </div>
 
-            <div className="buttons">
-                <div className="buttons left">
-                    Learn More
-                </div>
-                <div className="buttons right">
-                    Report!
-                </div>
+      <div className="heroSection">
+        <h2>Make Your City Better </h2>
+        <h3>
+          Voice your opinion and concerns to make your city a better place for
+          everyone
+        </h3>
+
+        <div className="buttons">
+          <div className="buttons left">Learn More</div>
+          <Link to="/report-form" style={{ textDecoration: "none" }}>
+            <div className="buttons right">Report</div>
+          </Link>
+        </div>
+      </div>
+
+      <div className="toDo">
+        <h1>What You Can Do...</h1>
+        <div className="con">
+          <div className="con1">
+            <div className="text">
+              <div className="text a">
+                <MegaphoneIcon size={32} color="#0369a1" strokeWidth={1.5} />
+                Report Issue
+              </div>
+
+              <div className="text b">
+                Easily report problems in your neighborhood, from potholes to
+                broken streetlights.
+              </div>
+
+              <div className="text c">
+                Help improve your community by speaking up!
+              </div>
             </div>
+          </div>
 
+          <div className="con1">
+            <div className="text">
+              <div className="text a">
+                <ClipboardListIcon
+                  size={32}
+                  color="#0369a1"
+                  strokeWidth={1.5}
+                />
+                Track Progress
+              </div>
+
+              <div className="text b">
+                Follow the status of reported issues and see how your city is
+                addressing them.
+              </div>
+
+              <div className="text c">
+                Stay informed about the changes happening around you!
+              </div>
+            </div>
+          </div>
         </div>
 
-        <div className="toDo">
-            <h1>What You Can Do...</h1>
-            <div className="con">
-
-                <div className="con1">
-
-                <div className="text">
-
-                    <div className="text a">
-                    <MegaphoneIcon size={32} color= '#0369a1' strokeWidth={1.5} />
-                    Report Issue
-                    </div>
-
-                    <div className="text b">
-                    Easily report problems in your neighborhood, from potholes to broken streetlights.
-                    </div>
-
-                    <div className="text c">
-                    Help improve your community by speaking up!
-                    </div>
-
-                </div>
-                </div>
-
-                <div className="con1">
-
-                    <div className="text">
-
-                        <div className="text a">
-                        <ClipboardListIcon size={32} color= '#0369a1' strokeWidth={1.5} />
-
-                        Track Progress
-                        </div>
-
-                        <div className="text b">
-                        Follow the status of reported issues and see how your city is addressing them.
-                        </div>
-
-                        <div className="text c">
-                        Stay informed about the changes happening around you!
-                        </div>
-
-                    </div>
-                </div>
-
-
-            </div>
-
-
-            {/* <div className="con">
+        {/* <div className="con">
 
             <div className="con1">
 
@@ -130,55 +123,42 @@ function CoverPage(){
 
 
             </div> */}
+      </div>
 
-        </div>
+      <div className="commfor">
+        <h2 className="title">Community Forum</h2>
 
-        <div className="commfor">
-            <h2 className="title">Community Forum</h2>
+        <div className="card">
+          <div className="cardHeader">
+            <UsersIcon size={32} color="#0369a1" strokeWidth={1.5} />
 
-            <div className="card">
-                <div className="cardHeader">
-                <UsersIcon size={32} color= '#0369a1' strokeWidth={1.5} />
+            <div className="subject">Join the Conversation</div>
+          </div>
 
-                    <div className="subject">
+          <h2>
+            Connect with your neighbors, discuss local issues, and share ideas
+            for improving your community.
+          </h2>
 
-                    Join the Conversation
-                    </div>
-                </div>
-
-                <h2>
-                Connect with your neighbors, discuss local issues, and share ideas for improving your community.
-                </h2>
-
-                <div className="discus">
-                <h3>Latest Discussions</h3>
-                    <div className="item">
-                        
-                    <div className="list">
-                        <li>New bike lanes on Main Street</li>
-                        <li>Community garden proposal</li>
-                        <li>Upcoming town hall meeting</li>
-                    </div>
-
-                    </div>
-                    
-
-                </div>
-                <button>Enter Forum</button>
+          <div className="discus">
+            <h3>Latest Discussions</h3>
+            <div className="item">
+              <div className="list">
+                <li>New bike lanes on Main Street</li>
+                <li>Community garden proposal</li>
+                <li>Upcoming town hall meeting</li>
+              </div>
             </div>
-
+          </div>
+          <button>Enter Forum</button>
         </div>
+      </div>
 
-        <footer className="footer">
+      <footer className="footer">
         <p>&copy; 2024 UrbanVoice. All rights reserved.</p>
       </footer>
-
-   </div>
-
-    )
-
+    </div>
+  );
 }
-
-
 
 export default CoverPage;
